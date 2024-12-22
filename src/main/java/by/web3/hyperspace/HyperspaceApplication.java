9959b8ba98c6c691bca4091eab68c131629bcfca
DmitryPrus <dmitri.prus@365smartshop.com>
2024-12-22 10:59:12 +0300

    init

---
+++ src/main/java/by/web3/hyperspace/HyperspaceApplication.java
@@ -0,0 +1,13 @@
+package by.web3.hyperspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/web3/hyperspace/HyperspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.web3.hyperspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
