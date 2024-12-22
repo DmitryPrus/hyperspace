bdf7e565527ce40894cd08448b575b9760f61091
DmitryPrus <dmitri.prus@365smartshop.com>
2024-12-22 11:00:32 +0300

    second commit

--- src/main/java/by/web3/hyperspace/HyperspaceApplication.java
+++ src/main/java/by/web3/hyperspace/HyperspaceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HyperspaceApplication {
 
-	// comment to check git
 	public static void main(String[] args) {
 		SpringApplication.run(HyperspaceApplication.class, args);
 	}
